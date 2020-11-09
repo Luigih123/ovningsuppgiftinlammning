5ebe6687134cee571aea2092cfd79f65fc87bff4
Luigih123 <luigi.hansson@elev.ga.ntig.se>
2020-11-09 15:17:29 +0100

    Create inlämningsupg.py

---
+++ inlämningsupg.py
@@ -0,0 +1,8 @@
+import random
+Godtyckligt = random.randint(1,10)
+repeat = 0
+print(f'{Godtyckligt} slumpade tal')
+while Godtyckligt > repeat:
+    n = random.randint(1,100)
+    repeat = repeat + 1
+    print(n)
