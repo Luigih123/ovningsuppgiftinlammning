2da6ca71dc6a0dffc240fb0764d26f02906446db
Luigih123 <luigi.hansson@elev.ga.ntig.se>
2020-11-16 14:39:22 +0100

    yeah

--- inlämningsupg.py
+++ inlämningsupg.py
@@ -1,8 +1,8 @@
-import random
-Godtyckligt = random.randint(1,10)
-repeat = 0
-print(f'{Godtyckligt} slumpade tal')
-while Godtyckligt > repeat:
-    n = random.randint(1,100)
-    repeat = repeat + 1
-    print(n)
+mål = int(input('Ange ett heltal antal kroner som är ditt mål: ')) #Användaren skriver ett mål, alltså antal pengar den vill nå
+mål = mål*100 #Konverterar öre till kronor
+öre = 1 #Startvärde för öre
+dagar = 0 #Starvärde för dagar
+while öre <= mål: #Loopar programet tills öre är lika mycket eller mer än målet
+    dagar = dagar + 1 #Varje gång programent gör en loop så lägger den till 1 dag på antal dagar
+    öre = öre*2 #Lönen dubblas varje gång den gör en loop
+print(f'Det tog {dagar} dagar tills du uppnådde ditt mål på {mål/100:.0f} kr! ') #Den skriver ut hur många dagar det tog och hur mycket pengar du fick
